0f0c5f41cc58de6c48f7afb900db31992ec24d60
jkornat <jakub@kornatow.ski>
2021-01-01 16:43:47 +0100

    Initial Commit

---
+++ Shared/FrugaleeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrugaleeApp.swift
+//  Shared
+//
+//  Created by Jakub Kornatowski on 01/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct FrugaleeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
